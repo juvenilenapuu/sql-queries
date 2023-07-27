3198e92cab7597614b8745239173fa93205ddb4c
@juveNile_ <juvinilenapuu@gmail.com>
2023-07-27 16:42:38 +0300

    join

--- queries.sql
+++ queries.sql
@@ -162,6 +162,10 @@ FROM table1
 INNER JOIN table2
 ON table1.column_name = table2.column_name;
 
-
+//LEFT JOIN Syntax
+SELECT column_name(s)
+FROM table1
+LEFT JOIN table2
+ON table1.column_name = table2.column_name;
 
 
