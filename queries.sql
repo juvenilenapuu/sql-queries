0ef342bda56db03d658678be445221816667a7bf
@juveNile_ <juvinilenapuu@gmail.com>
2023-07-27 16:43:00 +0300

    join

--- queries.sql
+++ queries.sql
@@ -168,4 +168,10 @@ FROM table1
 LEFT JOIN table2
 ON table1.column_name = table2.column_name;
 
+//RIGHT JOIN Syntax
+SELECT column_name(s)
+FROM table1
+RIGHT JOIN table2
+ON table1.column_name = table2.column_name;
+
 
