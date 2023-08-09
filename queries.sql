f3ae22419eec272e717375fc0825aa8177718f25
Juve-Nile <juvinilenapuu@gmail.com>
2023-08-09 14:46:34 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -239,3 +239,16 @@ WHERE column_name operator ALL
   (SELECT column_name
   FROM table_name
   WHERE condition);
+
+//SELECT INTO Syntax
+Copy all columns into a new table:
+SELECT *
+INTO newtable [IN externaldb]
+FROM oldtable
+WHERE condition;
+
+//Copy only some columns into a new table:
+SELECT column1, column2, column3, ...
+INTO newtable [IN externaldb]
+FROM oldtable
+WHERE condition;
