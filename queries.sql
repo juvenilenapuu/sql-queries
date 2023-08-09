cee8c53da372f0bd9d45bf971c9a3086a230bed3
Juve-Nile <juvinilenapuu@gmail.com>
2023-08-09 14:49:20 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -252,3 +252,23 @@ SELECT column1, column2, column3, ...
 INTO newtable [IN externaldb]
 FROM oldtable
 WHERE condition;
+
+//INSERT INTO SELECT Syntax
+Copy all columns from one table to another table:
+INSERT INTO table2
+SELECT * FROM table1
+WHERE condition;
+
+//Copy only some columns from one table into another table:
+INSERT INTO table2 (column1, column2, column3, ...)
+SELECT column1, column2, column3, ...
+FROM table1
+WHERE condition;
+
+//CASE Syntax
+CASE
+    WHEN condition1 THEN result1
+    WHEN condition2 THEN result2
+    WHEN conditionN THEN resultN
+    ELSE result
+END;
